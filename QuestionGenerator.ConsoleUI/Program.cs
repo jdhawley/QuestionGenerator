8c8c998079ff17346af79338791b154a3d20cf67
Jonathan <jdhawley@crimson.ua.edu>
2019-03-03 11:12:02 -0600

    Added files containing unprocessed questions and a class that will process the data

--- QuestionGenerator.ConsoleUI/Program.cs
+++ QuestionGenerator.ConsoleUI/Program.cs
@@ -8,6 +8,15 @@ namespace QuestionGenerator.ConsoleUI
     {
         static void Main(string[] args)
         {
+            using (LocalDbContext context = new LocalDbContext())
+            {
+                if(context.Questions.Count() == 0)
+                {
+                    SeedData.SeedBookOfQuestions(@"C:\Users\Jonathan\source\repos\QuestionGenerator\QuestionGenerator.Data\UnprocessedSeedData\BookOfQuestions.txt");
+                    //SeedData.SeedQuestionsForCouples(@"C:\Users\Jonathan\source\repos\QuestionGenerator\QuestionGenerator.Data\UnprocessedSeedData\QuestionsForCouples.txt");
+                }
+            }
+
             //TODO: Pull 3 random questions
             //TODO: Send questions to Katie and I
         }
---
+++ QuestionGenerator.Data/SeedData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuestionGenerator.Data
+{
+    public class SeedData
+    {
+        public static void SeedQuestionsForCouples(string filepath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void SeedBookOfQuestions(string filepath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
