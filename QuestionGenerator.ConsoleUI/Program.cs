5ab03ad5948707f02454d4ecc3303fc5232a4bd1
Jonathan <jdhawley@crimson.ua.edu>
2019-03-03 13:07:53 -0600

    Added query to retrieve 3 random, unused questions

--- QuestionGenerator.ConsoleUI/Program.cs
+++ QuestionGenerator.ConsoleUI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using QuestionGenerator.Data;
+using QuestionGenerator.Domain;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace QuestionGenerator.ConsoleUI
@@ -15,7 +17,21 @@ namespace QuestionGenerator.ConsoleUI
                     SeedData.SeedQuestions(@"C:\Users\Jonathan\source\repos\QuestionGenerator\QuestionGenerator.Data\ProcessedSeedData\BookOfQuestions.txt");
                     SeedData.SeedQuestions(@"C:\Users\Jonathan\source\repos\QuestionGenerator\QuestionGenerator.Data\ProcessedSeedData\QuestionsForCouples.txt");
                 }
+
+                List<Question> randomQuestions = context.Questions
+                                                        .Where(q => q.DateUsedUTC == null)
+                                                        .OrderByDescending(q => q.PreferredQuestion)
+                                                        .ThenBy(r => Guid.NewGuid())
+                                                        .Take(3)
+                                                        .ToList();
+
+                foreach(Question question in randomQuestions)
+                {
+                    Console.WriteLine(question.QuestionText);
+                }
             }
+
+
 
             //TODO: Pull 3 random questions
             //TODO: Send questions to Katie and I
