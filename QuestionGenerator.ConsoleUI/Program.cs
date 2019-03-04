cd40ce75d045d0ad0fa25fc7e87bfc99cf3f98d6
Jonathan <jdhawley@crimson.ua.edu>
2019-03-04 07:09:49 -0600

    Added email settings to appsettings.json Added appsettings.json to .gitignore and added appsettings.sample.json to show how to configure appsettings.json

--- QuestionGenerator.ConsoleUI/Program.cs
+++ QuestionGenerator.ConsoleUI/Program.cs
@@ -3,6 +3,10 @@ using QuestionGenerator.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace QuestionGenerator.ConsoleUI
 {
@@ -10,9 +14,21 @@ namespace QuestionGenerator.ConsoleUI
     {
         static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            List<string> mailgunTo = new List<string>();
+            string mailgunApiKey = configuration.GetSection("mailgun-apikey").Value;
+            string mailgunDomain = configuration.GetSection("mailgun-domain").Value;
+            string mailgunFrom = configuration.GetSection("mailgun-from").Value;
+            configuration.GetSection("mailgun-to").Bind(mailgunTo);
+
             using (LocalDbContext context = new LocalDbContext())
             {
-                if(context.Questions.Count() == 0)
+                if (context.Questions.Count() == 0)
                 {
                     SeedData.SeedQuestions(@"C:\Users\Jonathan\source\repos\QuestionGenerator\QuestionGenerator.Data\ProcessedSeedData\BookOfQuestions.txt");
                     SeedData.SeedQuestions(@"C:\Users\Jonathan\source\repos\QuestionGenerator\QuestionGenerator.Data\ProcessedSeedData\QuestionsForCouples.txt");
@@ -25,16 +41,32 @@ namespace QuestionGenerator.ConsoleUI
                                                         .Take(3)
                                                         .ToList();
 
-                foreach(Question question in randomQuestions)
+                string questions = "";
+                foreach (Question question in randomQuestions)
                 {
-                    Console.WriteLine(question.QuestionText);
+                    questions += question.QuestionText + "\n";
                 }
+
+                RestClient client = new RestClient();
+                client.BaseUrl = new Uri("https://api.mailgun.net/v3");
+                client.Authenticator =
+                    new HttpBasicAuthenticator("api",
+                                                mailgunApiKey);
+                RestRequest request = new RestRequest();
+                request.AddParameter("domain", mailgunDomain, ParameterType.UrlSegment);
+                request.Resource = $"{mailgunDomain}/messages";
+                request.AddParameter("from", mailgunFrom);
+                foreach (string emailTo in mailgunTo)
+                    request.AddParameter("to", emailTo);
+                request.AddParameter("subject", $"Email Questions for {DateTime.Today.ToString("MM/dd/yyyy")}");
+                request.AddParameter("text", questions);
+                request.Method = Method.POST;
+                Console.WriteLine(client.Execute(request));
             }
 
 
 
-            //TODO: Pull 3 random questions
-            //TODO: Send questions to Katie and I
+            //TODO: Update the questions to show they have been sent.
 
             Console.WriteLine("Execution complete. Press enter to close.");
             Console.ReadLine();
