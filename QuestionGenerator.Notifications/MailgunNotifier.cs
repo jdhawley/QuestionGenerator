7462e77e6b6d32f662401a8725d5d1170306488a
Jonathan <jdhawley@crimson.ua.edu>
2019-03-17 07:46:02 -0500

    Added functionality to MailgunNotifier to allow refactoring of the main program

--- QuestionGenerator.Notifications/MailgunNotifier.cs
+++ QuestionGenerator.Notifications/MailgunNotifier.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace QuestionGenerator.Notifications
 {
     public class MailgunNotifier : INotifier
     {
         public string From { get; set; }
-        public string Recipients { get; set; }
+        public List<string> Recipients { get; set; }
         public string Subject { get; set; }
         public string Domain { get; set; }
         public string ApiKey { get; set; }
@@ -24,22 +27,36 @@ namespace QuestionGenerator.Notifications
 
         public void SendMessage(string message)
         {
-            throw new NotImplementedException();
+            RestClient client = new RestClient();
+            client.BaseUrl = new Uri("https://api.mailgun.net/v3");
+            client.Authenticator = new HttpBasicAuthenticator("api", ApiKey);
+
+            RestRequest request = new RestRequest();
+            request.AddParameter("domain", Domain, ParameterType.UrlSegment);
+            request.Resource = $"{Domain}/messages";
+            foreach (string emailTo in Recipients)
+                request.AddParameter("to", emailTo);
+            request.AddParameter("from", From);
+            request.AddParameter("subject", $"Email Questions for {DateTime.Today.ToString("MM/dd/yyyy")}");
+            request.AddParameter("text", message);
+            request.Method = Method.POST;
+            
+            client.Execute(request);
         }
 
         public void SetFrom(string from)
         {
-            throw new NotImplementedException();
+            From = from;
         }
 
         public void SetRecipients(IEnumerable<string> recipients)
         {
-            throw new NotImplementedException();
+            Recipients = recipients.ToList();
         }
 
         public void SetSubject(string subject)
         {
-            throw new NotImplementedException();
+            Subject = subject;
         }
     }
 }
